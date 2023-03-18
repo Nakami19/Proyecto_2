406f0566f7fe73644f0326875edf0b3209d269b5
Tomás <tommygilbravo@gmail.com>
2023-03-18 19:37:35 -0400

    Inicio del Proyecto #2

---
+++ Proyecto#2/src/Main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import Interfaces.Ventana_1;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana_1 ventana = new Ventana_1();
+        ventana.setVisible(true);
+    }
+    
+}
